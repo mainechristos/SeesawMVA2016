b2ee29f8d12080e383e882dae7f353a48c32a876
Maine Christos <mchristos@hexcms.hexfarm.rutgers.edu>
2019-03-20 00:42:02 -0400

    run trainings and application

---
+++ run_signal_3L_2016.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# reads signal input file and generates new file with BDT variable
+#mkdir /cms/mchristos/ANN/Seesaw/2016/92X/outFiles/SeesawTMVAClassificationApplication$1/Background
+source /cvmfs/sft.cern.ch/lcg/views/LCG_94/x86_64-slc6-gcc7-opt/setup.sh
+export DISPLAY=localhost:0.0
+mkdir outFiles/Application_$1 
+echo $2
+mkdir outFiles/Application_$1/$2
+cp Application_2016_signal.C outFiles/Application_$1/$2/Application_2016_signal.C
+sed -i "s/QQQ/$2/g;" outFiles/Application_$1/$2/Application_2016_signal.C
+sed -i "s/JJJ/$1/g;" outFiles/Application_$1/$2/Application_2016_signal.C
+root -l -q outFiles/Application_$1/$2/Application_2016_signal.C
